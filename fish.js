26d102284ede28e4c7c1ba70045f80bb2f2f9404
Stuart <gravatar@strudel.org.uk>
2017-01-06 20:38:04 +0000

    Adapt aspect ratio

--- fish.js
+++ fish.js
@@ -16,9 +16,11 @@ S(document).ready(function(){
 
 function Fish(id){
 	
-	this.paper = new SVG('fish');
+	this.id = id;
+	this.paper = new SVG(this.id);
 	w = this.paper.w;
 	h = this.paper.h;
+	this.aspectratio = w/h;
 
 	this.rounded = Math.random();
 	
@@ -46,6 +48,20 @@ function Fish(id){
 		return Math.min(Math.max(lo,Math.random()),hi);
 	}
 
+	var _obj = this;
+	// We'll need to change the sizes when the window changes size
+	window.addEventListener('resize', function(event){ _obj.resize(); });
+
+	this.resize = function(){
+		S('#'+this.id).css({'height':''});
+		w = S('#'+this.id)[0].offsetWidth;
+		S('#'+this.id).css({'height':(w/this.aspectratio)+'px'});
+		this.paper = new SVG(this.id);
+		w = this.paper.w;
+		h = this.paper.h;
+		this.paper.clear();
+		this.draw();
+	}
 	this.update = function(){ this.create().draw(); }
 	this.create = function(){
 
@@ -58,27 +74,27 @@ function Fish(id){
 		points = new Array(10);
 
 		// Start with nose
-		points[0] = { x: Math.round(w*0.1), y:(h/2) - h*front*(Math.random()-0.5)*0.5 };
-		points[1] = { x: points[0].x, y:points[0].y - h*front*Math.random()*0.2 };
+		points[0] = { x: 0.1, y: 0.5 - front*(Math.random()-0.5)*0.5 };
+		points[1] = { x: points[0].x, y: points[0].y - front*Math.random()*0.2 };
 
 		// Slightly move top of nose forwards
-		points[1].x -= w*(1-Math.pow(front,2))*0.02;
+		points[1].x -= (1-Math.pow(front,2))*0.02;
 
 		// Add head
-		points[2] = {x:Math.round(w*0.3),y:(h*(1 - front)/2)};
-		points[9] = {x:Math.round(w*0.3),y:(h*(1 + front)/2)};
+		points[2] = {x: 0.3, y:((1 - front)/2)};
+		points[9] = {x: 0.3, y:((1 + front)/2)};
 
 		// Add end of body
-		points[3] = {x:Math.round(w*0.7),y:Math.round((h*(1 - back)/2))};
-		points[8] = {x:Math.round(w*0.7),y:Math.round(h - h*0.2)};
+		points[3] = {x: 0.7, y: ((1 - back)/2)};
+		points[8] = {x: 0.7, y: 0.8};
 
 		// Add start of tail
-		points[4] = {x:Math.round(w*0.83),y:h-Math.round(h*0.55)};
-		points[7] = {x:Math.round(w*0.83),y:h-Math.round(h*0.45)};
+		points[4] = {x: 0.83, y: 0.45 };
+		points[7] = {x: 0.83, y: 0.55 };
 
 		// Add start of tail
-		points[5] = {x:Math.round(w*0.9),y:h-Math.round(h*0.6)};
-		points[6] = {x:Math.round(w*0.9),y:h-Math.round(h*0.4)};
+		points[5] = {x: 0.9,y: 0.4 };
+		points[6] = {x: 0.9,y: 0.6 };
 
 		eye = { x: points[0].x + (points[2].x-points[0].x)*0.75, y: (points[1].y - inRange(0,0.6)*Math.abs(points[2].y - points[1].y)) };
 
@@ -96,40 +112,40 @@ function Fish(id){
 		for(var i = 0; i <= points.length; i++){
 			if(i == 2){
 				path += 'C';
-				dx = -0.2*w*this.rounded;
+				dx = -0.2 * this.rounded;
 				dy = bodygrad1*dx;
-				if(Math.abs(points[2].y - points[1].y) < 30){
+				if(Math.abs(points[2].y - points[1].y) < 0.05){
 					dx = 0;
 					dy = 0;
 				}
-				path += Math.round(points[i-1].x)+','+Math.round(points[i-1].y)+' '+Math.round(points[i].x + dx)+','+Math.round(points[i].y + dy)+' '+Math.round(points[i].x)+','+Math.round(points[i].y);
+				path += Math.round(w*(points[i-1].x))+','+Math.round(h*(points[i-1].y))+' '+Math.round(w*(points[i].x + dx))+','+Math.round(h*(points[i].y + dy))+' '+Math.round(w*points[i].x)+','+Math.round(h*points[i].y);
 			}else if(i == 4){
 				path += 'C';
-				dx = 0.1*w*this.rounded;
+				dx = 0.1*this.rounded;
 				dy = bodygrad1*dx;
-				path += Math.round(points[i-1].x + dx)+','+Math.round(points[i-1].y + dy)+' '+Math.round(points[i].x)+','+Math.round(points[i].y)+' '+Math.round(points[i].x)+','+Math.round(points[i].y);
+				path += Math.round(w*(points[i-1].x + dx))+','+Math.round(h*(points[i-1].y + dy))+' '+Math.round(w*points[i].x)+','+Math.round(h*points[i].y)+' '+Math.round(w*points[i].x)+','+Math.round(h*points[i].y);
 			}else if(i == 8){
 				path += 'C';
-				path += Math.round(points[i-1].x)+','+Math.round(points[i-1].y)+' '+Math.round(points[i].x+0.1*w*this.rounded)+','+Math.round(points[i].y)+' '+Math.round(points[i].x)+','+Math.round(points[i].y);
+				path += Math.round(w*points[i-1].x)+','+Math.round(h*points[i-1].y)+' '+Math.round(w*(points[i].x+0.1*this.rounded))+','+Math.round(h*points[i].y)+' '+Math.round(w*points[i].x)+','+Math.round(h*points[i].y);
 			}else if(i == 10){
 				path += 'C';
-				dx = -0.15*w*this.rounded;
+				dx = -0.15*this.rounded;
 				dy = bodygrad2*dx;
-				if(Math.abs(points[0].y - points[9].y) < 30){
+				if(Math.abs(points[0].y - points[9].y) < 0.05){
 					dx = 0;
 					dy = 0;
 				}
-				path += Math.round(points[i-1].x + dx)+','+Math.round(points[i-1].y + dy)+' '+Math.round(points[0].x)+','+Math.round(points[0].y)+' '+Math.round(points[0].x)+','+Math.round(points[0].y);
+				path += Math.round(w*(points[i-1].x + dx))+','+Math.round(h*(points[i-1].y + dy))+' '+Math.round(w*points[0].x)+','+Math.round(h*points[0].y)+' '+Math.round(w*points[0].x)+','+Math.round(h*points[0].y);
 			}else{
 				path += 'L';
-				path += Math.round(points[i].x)+','+Math.round(points[i].y);
+				path += Math.round(w*points[i].x)+','+Math.round(h*points[i].y);
 			}
 		}	
 		path += '';
 
 		this.paper.path(path).attr({'stroke':0,'fill':this.colour});
-		this.paper.circle(eye.x, eye.y, w*0.02).attr({'stroke':0,'fill':'white'});
-		this.paper.circle(eye.x, eye.y, w*0.015).attr({'stroke':0,'fill':'black'});
+		this.paper.circle(eye.x*w, eye.y*h, w*0.02).attr({'stroke':0,'fill':'white'});
+		this.paper.circle(eye.x*w, eye.y*h, w*0.015).attr({'stroke':0,'fill':'black'});
 		
 		this.paper.draw();
 		return this;
